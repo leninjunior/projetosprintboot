a013906d954dce8a2cad0ca06bbe05e39b877138
Lenin Junior <leninbessa@gmail.com>
2021-01-18 18:11:57 -0400

    Criando operação de instanciação no banco de dados

--- src/main/java/com/leninjunior/projeto/ProjetoApplication.java
+++ src/main/java/com/leninjunior/projeto/ProjetoApplication.java
@@ -1,13 +1,33 @@
 package com.leninjunior.projeto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoApplication {
+import com.leninjunior.projeto.domain.Categoria;
+import com.leninjunior.projeto.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
